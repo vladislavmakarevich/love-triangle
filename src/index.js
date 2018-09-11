62a568a9f840e6d1dcafac2f2ea31f0689b56f77
vladislavmakarevich <bgmakar79@gmail.com>
2018-09-11 21:15:58 +0300

    'Second'

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count=0;
+  for(var i=0;i<preferences.length;i++){
+    var k = preferences[i];
+    var m = preferences[k-1];
+    var n = preferences[m-1];
+    if(n-1===i){
+      count++;
+    }
+  }
+  return Math.floor(count/3);
 };
